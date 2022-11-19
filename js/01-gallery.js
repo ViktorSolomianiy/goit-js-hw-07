eea77da1ccf03b12c6986a0941901ed60776e82a
ViktorSolomianiy <xmegasystem35@gmail.com>
2022-11-19 17:04:15 +0200

    comlited hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const cardsContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCards(galleryItems);
+
+cardsContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+cardsContainer.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(evt.target.nodeName);
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  cardsContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function createGalleryCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const cardsContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCards(galleryItems);
+
+cardsContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+cardsContainer.addEventListener("click", onClick);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(evt.target.nodeName);
+}
+
+function createGalleryCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+         <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+         </a>`;
+    })
+    .join("");
+}
